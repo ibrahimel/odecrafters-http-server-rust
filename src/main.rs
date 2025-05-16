86cabbe6e8380148fb36d1458cfe1665468798fc
Ibrahim EL RHEZZALI <ibrahim.el@pm.me>
2025-05-16 13:29:48 +0200

    echo step debug

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,7 @@ fn main() {
                 // Different resonses and requets
                 let response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
                 let mut request = [0u8; 1024];
-                //let mut request_string: String = String::new();
                 let request_head = "GET /echo/";
-                let request_tail = "HTTP/1.1\r\nHost: localhost:4221\r\n\r\n";
 
                 // Read the request data
                 let _size = stream.read(&mut request).unwrap();
@@ -28,14 +26,9 @@ fn main() {
                 // Compare the requests data with the partial expected for a 200, otherwise respond with a 404
                 if request_string.starts_with(request_head) {
                     let partial = request_string.strip_prefix(request_head).unwrap();
-                    let (head, tail) = partial.split_once(" ").unwrap();
-                    if request_tail.eq(tail) {
-                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", head.len(), head);
-                        stream.write(response.as_bytes()).unwrap();
-                    } else {
-                        println!("False tail");
-                        stream.write(response_404.as_bytes()).unwrap();
-                    }
+                    let (head, _) = partial.split_once(" ").unwrap();
+                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", head.len(), head);
+                    stream.write(response.as_bytes()).unwrap();
                 } else {
                     println!("False head");
                     stream.write(response_404.as_bytes()).unwrap();
